f8a42fb2dc98d9e86b77a2df93c7ecaf505b612b
Anankaaa <hegyaljai83@hotmail.com>
2022-10-25 19:26:12 +0200

    ready

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -13,15 +13,16 @@ export class SorterPipe implements PipeTransform {
    */
   transform(value: any[], key: string): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
+
 
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb vagy nincs megadva a key,
      * térj vissza a value változóval.
      */
-
-
+     if(!Array.isArray(value) || !key){
+      return value;
+    }
 
     /**
      * FELADAT!
@@ -33,7 +34,15 @@ export class SorterPipe implements PipeTransform {
      *  összehasonlításának az eredményével.
      */
 
-
+    return value.sort((a, b) => {
+      if(typeof a[key] ==='number' && typeof b[key] === 'number'){
+        return a[key] - b[key];
+      } else {
+        const aToString = String(a[key]);
+        const bToString = String(b[key]);
+        return aToString.localeCompare(bToString);
+      }
+    })
   }
 
 }
